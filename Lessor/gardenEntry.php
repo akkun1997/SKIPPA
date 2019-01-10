90e92a2d3a49eaf99b13ea637b2c2360e296c455
OKKHI <kd1201019@st.kobedenshi.ac.jp>
2019-01-10 10:35:24 +0900

    期間項目の変更

--- Lessor/gardenEntry.php
+++ Lessor/gardenEntry.php
@@ -15,12 +15,14 @@ if($_SESSION['userName']==="ゲスト"){
   $purpose = "遊ぶ";
  ?>
   <p>庭で登録中です</p>
-	<form method="POST" action="./gardenEntry_db.php">
+	<form method="POST" action="./gardenEntry_db.php" enctype="multipart/form-data">
     <table>
         <tr><td>目的</td><td><input type="text" name="purpose" ></td></tr>
     		<tr><td>郵便番号</td><td><input type="text" name="zip" ></td></tr>
     		<tr><td>住所</td><td><input type="text" name="address" ></td></tr>
-    		<tr><td>貸出期間</td><td><input type="date"  name="rental"  max="9999-12-31"></td></tr>
+    		<tr><td>貸出期間</td><td><input type="date"  name="startPeriod"  max="9999-12-31">
+				~
+				<input type="date" name="lastPeriod" max="9999-12-31"></td></tr>
     		<tr><td>料金(1時間単位)</td><td><input type="text" name="kane"></td></tr>
         <tr><td>写真</td><td><input type="file" name="gazou"></td></tr>
         <tr><td colspan="2"><input type="submit" value="送信"></td></tr>
