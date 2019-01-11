9c0df374bd5d1bd2bad80eaaac1bc7780b832bf8
OKKHI <kd1201019@st.kobedenshi.ac.jp>
2019-01-11 10:37:06 +0900

    ユーザー登録処理

--- classes/user.php
+++ classes/user.php
@@ -1,7 +1,6 @@
 <?php
 	// スーパークラスであるDbDataを利用するため
   	require_once __DIR__ . '/dbdata.php';
-
   	class User extends DbData{
   		//ログイン認証
   		public function authUser($userId,$password){
@@ -9,9 +8,8 @@
   			$stmt = $this->query($sql, [$userId,$password]);
   			return $stmt->fetch();
   		}
-
       //ユーザー登録処理
-      public function signUp($userId,$userName,$kana,$zip,$address,$tel,$password,$tempId){
+      public function signUp($userId,$userName,$kana,$nickName,$zip,$address,$tel,$license,$password,$tempId){
           $sql = "select * from users where userId=?";
           $stmt = $this->query($sql,[$userId]);
           $result = $stmt->fetch();
@@ -19,23 +17,20 @@
           if($result['userId']){
             return 'この'.$userId.'は既に登録されています。';
           }
-          $sql = "insert into users(userId,userName,kana,zip,address,tel,password) values(?,?,?,?,?,?,?)";
-          $result = $this->exec($sql, [$userId,$userName,$kana,$zip,$address,$tel,$password]);
-
+          $sql = "insert into users(userId,userName,kana,nickName,zip,address,tel,license,password) values(?,?,?,?,?,?,?,?,?)";
+          $result = $this->exec($sql, [$userId,$userName,$kana,$nickName,$zip,$address,$tel,$license,$password]);
           if($result){
             //登録に成功した場合、cart内に保存されている商品があれば登録したユーザーIDに変更する(ログイン時と同じ処理)
-            $this->changeCartUserId($tempId,$userId);
+            //$this->changeCartUserId($tempId,$userId);
             return "";
           }else{
             //失敗した場合
             return '新規登録できませんでした。管理者にお問い合わせください。';
           }
       }
-
-      public function updateUser($userId,$userName,$kana,$zip,$address,$tel,$password,$tempId){
-          $sql = "update users set userId=?,userName=?,kana=?,zip=?,address=?,tel=?,password=? where userId=?";
-          $result=$this->exec($sql,[$userId,$userName,$kana,$zip,$address,$tel,$password,$tempId]);
-
+      public function updateUser($userId,$userName,$kana,$nickName,$zip,$address,$tel,$license,$password,$tempId){
+          $sql = "update users set userId=?,userName=?,kana=?,nickName=?,zip=?,address=?,tel=?,license=?,password=? where userId=?";
+          $result=$this->exec($sql,[$userId,$userName,$kana,$nickName,$zip,$address,$tel,$license,$password,$tempId]);
           /*if($result){
             //更新に成功したが、Cart内に仮のユーザーIDの商品が入っていた場合、新しいユーザーIDに置き換えるまた、過去の注文履歴のユーザーIDも新しいものに置き換える
             if($userId != $tempId){
@@ -47,7 +42,6 @@
             return 'ユーザー情報の更新ができませんでした。管理者にお問い合わせください';
           }*/
       }
-
       /*ユーザーIDを変更した場合、過去の注文履歴のユーザーIDを新しいものに変更する
       public function changeOrderHistoryUserId($tempId,$userId){
         //Orderオブジェクトを生成し、注文履歴のユーザーIDを変更する
