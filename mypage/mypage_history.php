a6d341ec7e73462cd49d67d9b6601ece19513572
OKKHI <kd1201019@st.kobedenshi.ac.jp>
2019-01-11 10:40:10 +0900

    マイページ

---
+++ mypage/mypage_history.php
@@ -0,0 +1,17 @@
+<?php
+	require_once __DIR__ . '/../header.php';
+?>
+<!DOCTYPE html>
+<html lang = "ja">
+<head>
+    <p>契約履歴</p>
+</head>
+<body>
+
+
+
+    <a href="mypage.php" class = "square_btn">変更する</a>
+</body>
+<?php
+	require_once __DIR__ . '/../footer.php';
+?>
