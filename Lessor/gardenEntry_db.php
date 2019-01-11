e3f786e3c65426c9f12ecd054ef152452ea4f066
OKKHI <kd1201019@st.kobedenshi.ac.jp>
2019-01-11 10:33:16 +0900

    登録内容をデータベースへ

---
+++ Lessor/gardenEntry_db.php
@@ -0,0 +1,22 @@
+<?php
+
+function h($data){
+	return htmlspecialchars($data,ENT_QUOTES,"UTF-8");
+}
+
+session_start();
+
+	require_once __DIR__ . '/../header.php';
+
+?>
+
+<?PHP
+$userId =h($userId);
+$zip =h($_POST['zip']);
+$address = h($_POST['address']);
+$startPeriod = h($_POST['startPeriod']);
+$lastPeriod = h($_POST['lastPeriod']);
+$kane = h($_POST['kane']);
+$gazou = h($_POST['gazou']);
+$purpose = h($_POST['purpose']);
+?>
