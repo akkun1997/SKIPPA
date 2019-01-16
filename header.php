28732f8a1615afdb5012a3b5b582ce6280e1f34a
akkun <locorocoakkun@gmail.com>
2019-01-16 13:28:52 +0900

    CSS,headerno 変更

--- header.php
+++ header.php
@@ -13,22 +13,18 @@
 	<div class = "main">
 		<h1><a href = "<?= $index_php ?>" class="">SKIPPA</a></h1>
 		<P>ようこそ！<?= $nickName ?>さん</P>
-		<ui class = "navi">
+		<div class = "nav">
 			<li><a href = "<?= $index_php ?>">トップページ</a></li>
-			<li>|</li>
 			<li><a href = "<?= $search_php ?>">検索画面</a></li>
-			<li>|</li>
 			<li><a href = "<?= $entryCategory_php ?>">物件登録</a></li>
-			<li>|</li>
 			<li><a href = "<?= $skippaInfo_php ?>">SKIPPAとは</a></li>
-			<li>|</li>
 			<?php
 				if($userName === "ゲスト"){
 					echo '<li><a href = "' . $login_php . '">ログイン</a></li>';
 				}else{
 					echo '<li><a href = "' .$mypage_php . '">マイページ</a></li>';
-					echo '<li>|</li><li><a href = "' . $logout_php .'">ログアウト</a></li>';
+					echo '<li><a href = "' . $logout_php .'">ログアウト</a></li>';
 				}
 			?>
-		</ui>
+		</div>
 		<hr>
